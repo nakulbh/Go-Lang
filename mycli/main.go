7062c2de3b1318ca09336b171ae471c941fa3d65
nakulbh <nakulbhardwaj37@gmail.com>
2023-09-10 23:58:13 +0530

    Golang

--- mycli/main.go
+++ mycli/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -13,7 +14,22 @@ func main() {
 		Long:  `This CLI demonstrate how to create a CLI application with cobra.`,
 		Args:  cobra.MaximumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("Greetings,%s!\n", args[1])
+			fmt.Printf("Greetings,%s!\n", args[0])
 		},
+	}
+
+	helloCmd := &cobra.Command{
+		Use:   "hello",
+		Short: "Prints a 'Hello, world!' message",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello, world!")
+		},
+	}
+
+	rootCmd.AddCommand(helloCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }

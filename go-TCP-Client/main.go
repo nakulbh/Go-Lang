f40bea744afa1d5a879adf251cddb76a97aaca5a
nakulbh <nakulbhardwaj37@gmail.com>
2023-09-20 17:39:29 +0530

    Tcp client

--- go-TCP-Client/main.go
+++ go-TCP-Client/main.go
@@ -1,14 +1,29 @@
 package main
 
 import(
-	""
-)
-const(
-	Host = "localhost"
-	Port = "8080"
-	Type = "tcp"
+	"log"
+	"flag"
+	"net"
+	"bufio"
+	"os"
+	"fmt"
 )
 
 func main(){
-	tcpServer, err := net.ResolveTCPAddr(Type, Host+":"+Port)
+	const name = "WriteTcp"
+	log.SetPrefix(name + "\t")
+
+	port := flag.Int("p", 8080, "port to connect to")
+	flag.Parse()
+
+	conn, err := net.DialTCP("tcp",nil, &net.TCPAddr{Port: *port})
+	if err != nil {
+		log.Fatalf("error connecting to localhost:%d: %v,*port, err")
+	}
+	log.Printf("connected to %s: will forward stdin", conn.RemoteAddr())
+
+	defer conn.Close()
+	go func ()  {
+		
+	}
 }
